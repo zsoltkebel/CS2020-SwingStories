f1a5d7fe151f4bf31cfbfa3db866f521fb14a84a
Tom Utting <tom.utting1703@gmail.com>
2021-11-05 11:05:03 +0000

    Wrote the basic swing code

--- swingStory/src/main/java/uk/ac/abdn/comp/csd/App.java
+++ swingStory/src/main/java/uk/ac/abdn/comp/csd/App.java
@@ -1,20 +1,45 @@
 package uk.ac.abdn.comp.csd;
-
 import com.github.javafaker.*;
+import java.awt.BorderLayout;
+import javax.swing.*;
 
 /**
- * Hello world!
- *
+ * Tell me a story
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    private static String story(){
+    Faker faker = new Faker();
+    String name = faker.name().fullName();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String story = ( "Hello " + name + " " + firstName + " " + lastName );
+    return story;
+    }
+    private static void createAndShowGUI(){
+        //Create and set up the window
+        JFrame frame = new JFrame("Swing me a story");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        String story = story();
+        
+        //Add the labels
+        JLabel introLabel = new JLabel("The story is: ");
+        JLabel storyLabel = new JLabel(story);
+        frame.getContentPane().add(introLabel, BorderLayout.PAGE_START);
+        frame.getContentPane().add(storyLabel, BorderLayout.LINE_START);
+        
+        //Display the window.
+        frame.setSize(400,300);
+        frame.setVisible(true);
+    }
+    public static void main( String[] args)
     {
-        Faker faker = new Faker();
-         String name = faker.name().fullName();
-         String firstName = faker.name().firstName();
-         String lastName = faker.name().lastName();
-         System.out.println( "Hello World!" );
-         System.out.println( "Hello " + name + " " + firstName + " " + lastName );
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                createAndShowGUI();
+            }
+        });
     }
 }
